af5dbbca824381b6fd219ab4d3a53fda40efcb32
steveo <steveo@arista.com>
2025-04-06 11:11:57 -0700

    Convert it to podfeed!

--- application.py
+++ application.py
@@ -1,5 +1,8 @@
-from flask import Flask
-application = Flask(__name__)
+# from flask import Flask
+# application = Flask(__name__)
+
+# replace above with podfeed app. This should pull in the podfeed specific stuff
+from server import app as application
 
 @application.route('/')
 def hello_elastic_beanstalk():
